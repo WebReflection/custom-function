03761bb9de150f8613945e48dc6188a9da8e79b3
webreflection <andrea.giammarchi@gmail.com>
2023-02-16 07:27:18 +0100

    Added Closure Compiler friendly /closure export

---
+++ esm/closure.js
@@ -0,0 +1,16 @@
+/*! (c) Andrea Giammarchi - ISC */
+const {setPrototypeOf} = Object;
+
+/**
+ * Given a `class Test extends CustomFunction {}` a `new Test(() => {})`
+ * will returns the provided callback as `instanceof Test`.
+ * @param {function} fn the callback that will inherit class methods.
+ * @returns {function} the same, yet upgraded, `fn` callback.
+ */
+export default class CustomFunction {
+  constructor(fn) {
+    return setPrototypeOf(fn, this.constructor.prototype);
+  }
+}
+
+CustomFunction.prototype = Function.prototype;
